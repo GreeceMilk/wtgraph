1f056b6503a609b4c16fd05af4f5c33f5032883c
GreeceMilk <110751159+GreeceMilk@users.noreply.github.com>
2024-11-29 21:12:59 +0800

    Add basic dark mode

--- client/src/App.js
+++ client/src/App.js
@@ -2,56 +2,16 @@ import './App.css';
 import api from './api/axiosConfig';
 import {useState, useEffect} from 'react';
 import {Routes, Route} from 'react-router-dom'; 
-import {createTheme, ThemeProvider, useColorScheme} from '@mui/material/styles';
+import {createTheme, ThemeProvider} from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
 
 import Layout from './components/Layout';
 import Home from './components/Home';
 import NationGraph from './components/NationGraph';
 import VehicleGraph from './components/VehicleGraph';
 
-function App() {
-  const theme = createTheme(
-    {
-      colorSchemes: {
-        light: {
-          palette: {
-            primary: {
-              main: '#00838f',
-            },
-            secondary: {
-              main: '#ffc400',
-            },
-          },
-        },
-        dark: {
-          palette: {
-            primary: {
-              main: '#00838f',
-            },
-            secondary: {
-              main: '#ffc400',
-            },
-          },
-        },
-      }
-    }
-  )
-
-  const {mode, setMode} = useColorScheme();
-  const [t, setT] = useState();
-
-  async function getT() {
-    try {
-      const response = await api.get("/api/v1/test");
-      setT(response.data);
-      console.log(response.data);
-    } catch (error) {
-      console.log(error);
-    } 
-  }
-  // useEffect(() => {getT();},[]);
+function AppWorker() {
   return (
-    <ThemeProvider theme={theme} defaultMode='light'>
       <div className="App">
         <Routes>
           <Route path="/" element = {<Layout/>}>
@@ -61,8 +21,71 @@ function App() {
           </Route>
         </Routes>
       </div>
-    </ThemeProvider>
   );
+}
+
+const theme = createTheme(
+  {
+    colorSchemes: {
+      light: true, 
+      dark: true,
+      light: {
+        palette: {
+          primary: {
+            main: '#00838f',
+          },
+          secondary: {
+            main: '#ffc400',
+          },
+        },
+      },
+      dark: {
+        palette: {
+          primary: {
+            main: '#00838f',
+          },
+          secondary: {
+            main: '#ffc400',
+          },
+        },
+      },
+    }
+  }
+)
+
+function App() {
+
+  const light = createTheme({
+    palette: {
+      mode: 'light',
+      primary: {
+        main: '#1a2939',
+      },
+      secondary: {
+        main: '#d8a807',
+      },
+    },
+  });
+
+  const dark = createTheme({
+    palette: {
+      mode: 'dark',
+      primary: {
+        main: '#699bd8',
+      },
+      secondary: {
+        main: '#d8a807',
+      },
+    }
+  })
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      <AppWorker/>
+    </ThemeProvider>
+  )
+
 }
 
 export default App;
--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,13 +1,55 @@
 import {Outlet} from 'react-router-dom';    
-
-import React from 'react'
+import Box from '@mui/material/Box';
+import RadioGroup from '@mui/material/RadioGroup';
+import Radio from '@mui/material/Radio';
+import FormControl from '@mui/material/FormControl';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormLabel from '@mui/material/FormLabel';
+import { useColorScheme } from '@mui/material';
 
 const Layout = () => {
+  const {mode, setMode} = useColorScheme();
+
+  if (!mode) {
+    return (
+      <main>
+
+      </main>
+    );
+  }
+
   return (
     <main>
+        <Box
+    sx={{
+        display: 'flex',
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        bgcolor: 'background.default',
+        color: 'text.primary',
+        borderRadius: 1,
+        p: 3,
+        minHeight: '56px',
+      }}>
+       <FormControl>
+        <FormLabel id="demo-theme-toggle">Theme</FormLabel>
+        <RadioGroup
+          aria-labelledby="demo-theme-toggle"
+          name="theme-toggle"
+          row
+          value={mode}
+          onChange={(event) => setMode(event.target.value)}
+        >
+          <FormControlLabel value="system" control={<Radio />} label="System" />
+          <FormControlLabel value="light" control={<Radio />} label="Light" />
+          <FormControlLabel value="dark" control={<Radio />} label="Dark" />
+        </RadioGroup>
+      </FormControl>
+        </Box>
         <Outlet />
     </main>
-  )
+  );
 }
 
-export default Layout
+export default Layout;
