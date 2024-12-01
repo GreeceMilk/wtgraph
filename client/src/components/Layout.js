b187f94b7a12011b7a0674377dd6650f3bacb715
GreeceMilk <110751159+GreeceMilk@users.noreply.github.com>
2024-12-01 20:25:11 +0800

    add nav bar and darkmode switch

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,14 +1,47 @@
 import {Outlet} from 'react-router-dom';    
 import Box from '@mui/material/Box';
-import RadioGroup from '@mui/material/RadioGroup';
-import Radio from '@mui/material/Radio';
-import FormControl from '@mui/material/FormControl';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormLabel from '@mui/material/FormLabel';
-import { useColorScheme } from '@mui/material';
+import { AppBar, IconButton, Toolbar, Typography, useColorScheme, useMediaQuery } from '@mui/material';
+import { useEffect } from 'react';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import LightModeIcon from '@mui/icons-material/LightMode';
 
 const Layout = () => {
   const {mode, setMode} = useColorScheme();
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  
+
+  // useEffect(() => {
+    // console.log("Mode", mode);
+    // console.log("System perfer dark mode", prefersDarkMode);
+  // }, [mode])
+
+  function iconDisplay() {
+    if (mode === 'system') {
+      if (prefersDarkMode) {
+        return (<DarkModeIcon></DarkModeIcon>);
+      } else {
+        return (<LightModeIcon></LightModeIcon>);
+      }
+    } else if (mode === 'light') {
+      return (<LightModeIcon></LightModeIcon>);
+    } else {
+      return (<DarkModeIcon></DarkModeIcon>);
+    }
+  }
+
+  function modeSwitch() {
+    if (mode === 'system') {
+      if (prefersDarkMode) {
+        setMode('light');
+      } else {
+        setMode('dark');
+      }
+    } else if (mode === 'light') {
+      setMode('dark');
+    } else {
+      setMode('light');
+    }
+  }
 
   if (!mode) {
     return (
@@ -28,24 +61,22 @@ const Layout = () => {
         justifyContent: 'center',
         bgcolor: 'background.default',
         color: 'text.primary',
-        borderRadius: 1,
-        p: 3,
+        // borderRadius: 1,
+        // p: 0,
         minHeight: '56px',
       }}>
-       <FormControl>
-        <FormLabel id="demo-theme-toggle">Theme</FormLabel>
-        <RadioGroup
-          aria-labelledby="demo-theme-toggle"
-          name="theme-toggle"
-          row
-          value={mode}
-          onChange={(event) => setMode(event.target.value)}
-        >
-          <FormControlLabel value="system" control={<Radio />} label="System" />
-          <FormControlLabel value="light" control={<Radio />} label="Light" />
-          <FormControlLabel value="dark" control={<Radio />} label="Dark" />
-        </RadioGroup>
-      </FormControl>
+          <AppBar position="static" sx={{mb: 5}}>
+            <Toolbar disableGutters>
+              <Box sx={{flexGrow: 1}}>
+                <Typography>WTGRAPH</Typography>
+              </Box>
+              <Box sx={{flexGrow: 0, p: 2}}>
+                <IconButton onClick={modeSwitch}>
+                  {iconDisplay()}
+                </IconButton>
+              </Box>
+            </Toolbar>
+          </AppBar>
         </Box>
         <Outlet />
     </main>
