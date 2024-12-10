9b63dae0e6c4c5aea32b3c6331cfc5b568515a1e
GreeceMilk <110751159+GreeceMilk@users.noreply.github.com>
2024-12-10 16:12:32 +0800

    clean up unnecessary code

--- client/src/App.js
+++ client/src/App.js
@@ -10,19 +10,6 @@ import Home from './components/Home';
 import NationGraph from './components/NationGraph';
 import VehicleGraph from './components/VehicleGraph';
 
-function AppWorker() {
-  return (
-      <div className="App">
-        <Routes>
-          <Route path="/" element = {<Layout/>}>
-            <Route path="/" element={<Home/>}> </Route>
-            <Route path="/test" element={<NationGraph/>}></Route>
-            <Route path="/test1" element={<VehicleGraph/>}></Route>
-          </Route>
-        </Routes>
-      </div>
-  );
-}
 
 const theme = createTheme(
   {
@@ -54,35 +41,18 @@ const theme = createTheme(
 )
 
 function App() {
-
-  const light = createTheme({
-    palette: {
-      mode: 'light',
-      primary: {
-        main: '#1a2939',
-      },
-      secondary: {
-        main: '#d8a807',
-      },
-    },
-  });
-
-  const dark = createTheme({
-    palette: {
-      mode: 'dark',
-      primary: {
-        main: '#699bd8',
-      },
-      secondary: {
-        main: '#d8a807',
-      },
-    }
-  })
-
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline/>
-      <AppWorker/>
+      <div className="App">
+        <Routes>
+          <Route path="/" element = {<Layout/>}>
+            <Route path="/" element={<Home/>}> </Route>
+            <Route path="/test" element={<NationGraph/>}></Route>
+            <Route path="/test1" element={<VehicleGraph/>}></Route>
+          </Route>
+        </Routes>
+      </div>
     </ThemeProvider>
   )
 
