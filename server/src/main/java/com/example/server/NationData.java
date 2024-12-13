1065065aec02b97ae32bd193e8a0951e970290c1
GreeceMilk <110751159+GreeceMilk@users.noreply.github.com>
2024-12-13 18:10:04 +0800

    adjust nation data endpoint output structure

--- server/src/main/java/com/example/server/NationData.java
+++ server/src/main/java/com/example/server/NationData.java
@@ -6,6 +6,6 @@ import lombok.Data;
 
 @Data
 public class NationData {
-    private Date date;
+    private Date xData;
     private Float yData;
 }
--- server/src/main/java/com/example/server/NationRepo.java
+++ server/src/main/java/com/example/server/NationRepo.java
@@ -14,7 +14,7 @@ public interface NationRepo<T, ID> extends MongoRepository<T, ID> {
     Optional<T> findById(ObjectId id);
 
     // @Query(value = "{'nation': ?0, 'cls': ?1, 'ab_lower_br': ?3}", fields = "{'date' : 1, ?2: 1, '_id': 0}")
-    @Aggregation(pipeline = {"{$match: {'nation': ?0, 'cls': ?1, ?3: ?4}}", "{$project: {'date': 1, 'yData': '$?2', '_id': 0}}", "{$sort: {'date': 1}}"})
+    @Aggregation(pipeline = {"{$match: {'nation': ?0, 'cls': ?1, ?3: ?4}}", "{$project: {'xData': '$date', 'yData': '$?2', '_id': 0}}", "{$sort: {'xData': 1}}"})
     List<NationData> findByNationAndCls(String nation, String cls, String output, String mode, Double lowerBr);
 
     // @Aggregation(pipeline = {"{$group: {_id: '$nation'}}", "{$project: {'_id': 0, 'nation': '$_id'}}"})
