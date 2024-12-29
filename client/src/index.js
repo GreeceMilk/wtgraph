2b1872de87cc28ad8667b42b0837b0865e9a1560
GreeceMilk <110751159+GreeceMilk@users.noreply.github.com>
2024-12-29 20:01:24 +0800

    Fix crossOrigin attribute in preconnect link for fonts

--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <link rel="preconnect" href="https://fonts.googleapis.com"/>
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
     <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Monda:wght@400..700&family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"/>
 
     <BrowserRouter>
