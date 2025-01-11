d6aedc8700289c2aa887ae3002d1dc6f2b58542b
GreeceMilk <110751159+GreeceMilk@users.noreply.github.com>
2025-01-10 21:56:16 -0500

    add starter file for system tests

---
+++ system-tests/src/test/java/com/wtgraphtests/system_tests/SystemTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wtgraphtests.system_tests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
