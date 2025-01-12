af80383a3ce32e3518d2a8962692bb3c9805a5c0
GreeceMilk <110751159+GreeceMilk@users.noreply.github.com>
2025-01-11 23:30:34 -0500

    update api endpoints

--- client/src/api/axiosConfig.js
+++ client/src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'wtgraph-fjgcb0gzgabsbpcw.canadacentral-01.azurewebsites.net',
+    baseURL: 'https://wtgraph-fjgcb0gzgabsbpcw.canadacentral-01.azurewebsites.net',
     headers: { "ngrok-skip-browser-warning": "true" }
 });
